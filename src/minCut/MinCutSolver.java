4a494b7550ca34c2e2abdfc3c7de69023cb0a8e4
junming403 <wjmfelix0403@gmail.com>
2019-04-12 21:21:03 +0800

    fix bug in solbver

--- src/minCut/MinCutSolver.java
+++ src/minCut/MinCutSolver.java
@@ -1,29 +1,23 @@
 package minCut;
 
+import RandomGraphGenerator.randomGenerator;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
 public class MinCutSolver {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        MinCutSolver solver = new MinCutSolver("sample");
+    public static void main(String[] args) throws IOException {
+
+        // recursive version
+//        MinCutSolver solver = new MinCutSolver("sample");
+//        System.out.println("Min cut is " + solver.solve(3, 5));
+
         MinCutSolver solverIterative = new MinCutSolver("sample");
-
-//        System.out.println("Min cut is " + solver.solve(0, 899));
-//        System.out.println("Min cut is " + solverIterative.IterativeSolve(0, 899));
-
-        for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j < 1000; j++) {
-                if (i != j) {
-                    if (solver.solve(i, j) != solverIterative.solve(i, j)) {
-                        System.out.println("FAILED...");
-                    }
-                }
-            }
-        }
-        System.out.println("PASSED");
+        System.out.println("Min cut is " + solverIterative.IterativeSolve(3, 5));
 
     }
 
@@ -53,6 +47,35 @@ public class MinCutSolver {
         return totalFlow;
     }
 
+    private void printGraph() {
+        for (int i = 0; i < graph.getNodes().size(); i++) {
+            System.out.print(i + ": ");
+            for (int j = 0; j < graph.getNodes().get(i).getAdjList().size(); j++) {
+                System.out.print("(" + graph.getNodes().get(i).getAdjList().get(j) + "," + graph.getNodes().get(i).getWeights().get(j) + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    private void verifier() throws IOException {
+        int size = 200;
+        new randomGenerator().generate(size, 10000, "sample");
+        System.out.println();
+        for (int i = 0; i < size; i+=7) {
+            for (int j = 0; j < size; j+=3) {
+                if (i != j) {
+                    MinCutSolver solver = new MinCutSolver("sample");
+                    MinCutSolver solverIterative = new MinCutSolver("sample");
+                    if (solver.solve(i, j) != solverIterative.IterativeSolve(i, j)) {
+                        System.out.println("FAILED..." + i + "," + j);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("PASSED");
+    }
+
     public int IterativeSolve(int source, int dest) {
         int improvement = findResidualIterative(source, dest, new HashMap<>());
         while (improvement != Integer.MAX_VALUE) {
@@ -79,6 +102,7 @@ public class MinCutSolver {
         ArrayList<Integer> neighbours = node.getAdjList();
         for (int i = 0; i < neighbours.size(); i++) {
             int neighbour = neighbours.get(i);
+            Node neighbourNode = graph.getNodes().get(neighbour);
             int weight = node.getWeights().get(i);
             if (!visited.getOrDefault(neighbour, false)) {
                 if (weight > 0) {
@@ -91,11 +115,24 @@ public class MinCutSolver {
                     if (bottleNeck != Integer.MAX_VALUE) {
                         bottleNeck = Math.min(bottleNeck, weight);
                         node.getWeights().set(i, weight - bottleNeck);
+
+                        for (int j = 0; j < neighbourNode.getAdjList().size(); j++) {
+                            if (neighbourNode.getAdjList().get(j) == source) {
+                                if (neighbourNode.getWeights().get(j) < neighbourNode.getCapacity().get(j)) {
+                                    int diff = neighbourNode.getCapacity().get(j) - neighbourNode.getWeights().get(j);
+                                    if (diff > bottleNeck) {
+                                        neighbourNode.getWeights().set(j, neighbourNode.getWeights().get(j) + bottleNeck);
+                                    } else {
+                                        neighbourNode.getWeights().set(j, neighbourNode.getCapacity().get(j) + bottleNeck);
+                                        node.getWeights().set(i, weight - bottleNeck + diff);
+                                    }
+                                }
+                            }
+                        }
                         return bottleNeck;
                     }
                 }
 
-                Node neighbourNode = graph.getNodes().get(neighbour);
                 for (int j = 0; j < neighbourNode.getAdjList().size(); j++) {
                     if (neighbourNode.getAdjList().get(j) == source) {
                         if (neighbourNode.getWeights().get(j) < neighbourNode.getCapacity().get(j)) {
@@ -117,8 +154,9 @@ public class MinCutSolver {
     private int findResidualIterative(int source, int dest, HashMap<Integer, Boolean> visited) {
         Stack<Integer> searchStack = new Stack<>();
         Stack<Integer> bottleNeckStack = new Stack<>();
+        Stack<Integer> childPointerStack = new Stack<>();
         searchStack.push(source + 1);
-
+        childPointerStack.push(null);
         bottleNeckStack.push(Integer.MAX_VALUE);
 
         while(!searchStack.empty()) {
@@ -135,7 +173,6 @@ public class MinCutSolver {
 
             Node node = graph.getNodes().get(next);
 
-
             ArrayList<Integer> neighbours = node.getAdjList();
             for (int i = 0; i < neighbours.size(); i++) {
                 int neighbour = neighbours.get(i);
@@ -146,18 +183,28 @@ public class MinCutSolver {
                         if (neighbour != dest) {
                             searchStack.push(neighbour + 1);
                             bottleNeckStack.push(bottleNeck);
+                            childPointerStack.push(i);
                             isDeeper = true;
                             break;
                         } else {
                             int previous = dest;
+                            childPointerStack.push(i);
                             while (!searchStack.isEmpty()) {
+                                int curChildPointer = childPointerStack.pop();
                                 int k = searchStack.pop();
                                 if (k > 0) {
                                     k = k - 1;
                                     Node cur = graph.getNodes().get(k);
-                                    for (int j = 0; j < cur.getAdjList().size(); j++) {
-                                        if (cur.getAdjList().get(j) == previous) {
-                                            cur.getWeights().set(j, cur.getWeights().get(j) - bottleNeck);
+                                    Node neighbourNode = graph.getNodes().get(previous);
+                                    for (int i1 = 0; i1 < neighbourNode.getAdjList().size(); i1++) {
+                                        if (neighbourNode.getAdjList().get(i1) == k) {
+                                            int diff = neighbourNode.getCapacity().get(i1) - neighbourNode.getWeights().get(i1);
+                                            if (diff > bottleNeck) {
+                                                neighbourNode.getWeights().set(i1, neighbourNode.getWeights().get(i1) + bottleNeck);
+                                            } else {
+                                                neighbourNode.getWeights().set(i1, neighbourNode.getCapacity().get(i1));
+                                                cur.getWeights().set(curChildPointer, cur.getWeights().get(curChildPointer) - bottleNeck + diff);
+                                            }
                                             break;
                                         }
                                     }
@@ -176,13 +223,13 @@ public class MinCutSolver {
                             return bottleNeck;
                         }
                     }
-
                     Node neighbourNode = graph.getNodes().get(neighbour);
                     for (int j = 0; j < neighbourNode.getAdjList().size(); j++) {
                         if (neighbourNode.getAdjList().get(j) == source) {
                             if (neighbourNode.getWeights().get(j) < neighbourNode.getCapacity().get(j)) {
-                                searchStack.push(searchStack.pop() * -1);
+                                searchStack.push(-1 * searchStack.pop());
                                 searchStack.push(neighbour + 1);
+                                childPointerStack.push(i);
                                 bottleNeck = Math.min(bottleNeck,  neighbourNode.getCapacity().get(j) - neighbourNode.getWeights().get(j));
                                 bottleNeckStack.push(bottleNeck);
                                 isDeeper = true;
@@ -197,6 +244,7 @@ public class MinCutSolver {
             }
             if (!isDeeper) {
                 searchStack.pop();
+                childPointerStack.pop();
                 bottleNeckStack.pop();
             }
         }
