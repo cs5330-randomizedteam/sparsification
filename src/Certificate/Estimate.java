7e1ae43b754e48cac88e11cd10319432c0775790
junming <e0134140@u.nus.edu>
2019-04-16 12:13:18 +0800

    add debug

--- src/Certificate/Estimate.java
+++ src/Certificate/Estimate.java
@@ -3,12 +3,13 @@ package Certificate;
 import java.io.*;
 import java.util.*;
 
+import minCut.MinCutSolver;
 import util.Const;
 
 public class Estimate {
 
     public static void main(String[] args) throws Exception {
-        estimateGraph("starClique1");
+        estimateGraph("combined");
     }
 
     private static void estimateGraph(String fileName) throws Exception {
@@ -42,15 +43,32 @@ public class Estimate {
             }
         }
 
-        edges.forEach((k, v) -> {
+//        int exactMinCutSum = 0;
+//        int estimationSum = 0;
+//        MinCutSolver solver = new MinCutSolver("combined", false);
+//
+//        for (Edge edge: edges.keySet()) {
+//            if (edge.src % 100 == 0) {
+//                int estimation = edges.get(edge);
+//                int exact = solver.IterativeSolve(edge.src, edge.des);
+//                System.out.println("Estimation: " + estimation + " Exact: " + exact);
+//                estimationSum += estimation;
+//                exactMinCutSum += exact;
+//            }
+//        }
+//
+//        System.out.println("Exact min cut sum is " + exactMinCutSum);
+//        System.out.println("Estimation sum is " + estimationSum);
+
+//        edges.forEach((k, v) -> {
 //            System.out.printf("Estimation of edge (%d, %d) is %d\n", k.src, k.des, v);
-            if (k.src == 1 && k.des == 3) {
-                System.out.printf("Estimation of edge (%d, %d) is %d\n", k.src, k.des, v);
-            }
-            if (k.src == 0) {
-                System.out.printf("Estimation of edge (%d, %d) is %d\n", k.src, k.des, v);
-            }
-        });
+//            if (k.src == 1 && k.des == 3) {
+//                System.out.printf("Estimation of edge (%d, %d) is %d\n", k.src, k.des, v);
+//            }
+//            if (k.src == 0) {
+//                System.out.printf("Estimation of edge (%d, %d) is %d\n", k.src, k.des, v);
+//            }
+//        });
 
     }
 }
